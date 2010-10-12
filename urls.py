38ceafd43da8cc4e8a7d466cb2b61adb27144870
Daniel Roberts <daniel.mattos.roberts@gmail.com>
2010-10-12 02:58:23 +0000

    first commit

---
+++ urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls.defaults import *
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    (r'^', include('recipes_test.recipes.urls')),
+
+    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    (r'^admin/', include(admin.site.urls)),
+)
